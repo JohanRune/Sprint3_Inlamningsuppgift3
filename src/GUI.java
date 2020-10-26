28ddb81ae3bb29e7a2dc6cfc329fe5bc836639d3
JohanRune <kjgrune@yahoo.se>
2020-10-26 15:05:42 +0100

    Fungerar

--- src/GUI.java
+++ src/GUI.java
@@ -280,7 +280,7 @@ public class GUI extends JFrame implements ActionListener {
 
     public static void main(String[] args) {
         GUI g = new GUI();
-        g.GUI(false); //skriv true om det ska vara en kvar till seger.
+        g.GUI(true); //skriv true om det ska vara en kvar till seger.
 
     }
 
