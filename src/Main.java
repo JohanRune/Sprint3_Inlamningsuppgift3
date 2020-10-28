0211cc07751fbcbeb9da369c8b9a5a8e0e15a30a
JohanRune <kjgrune@yahoo.se>
2020-10-28 14:59:10 +0100

    Se om det branchar.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        GUI g = new GUI();
+        GUI g = new GUI(); //skrivet idag.
         g.GUI(false); //skriv true om det ska vara en kvar till seger, annars false
     }
 }
