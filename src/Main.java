01fbfb9f2974eb0b073767fa578c23421dbf6c55
JohanRune <kjgrune@yahoo.se>
2020-10-28 15:01:24 +0100

    Färdig version

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        GUI g = new GUI(); //ta bort
+        GUI g = new GUI();
         g.GUI(true); //skriv true om det ska vara en kvar till seger, annars false
     }
 }
