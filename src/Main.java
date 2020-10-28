622e9a6b2ae50760c14ba08b85fbdf1df2a06605
JohanRune <kjgrune@yahoo.se>
2020-10-28 14:07:10 +0100

    Lagt till text i main.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         GUI g = new GUI();
-        g.GUI(false); //skriv true om det ska vara en kvar till seger, annars false
+        g.GUI(true); //skriv true om det ska vara en kvar till seger, annars false
     }
 }
+
+//Master3
